f3c7cdd8275491fb106ca5fc2bf7d9bc8a3138d1
Bowie <rafael.ps.skt@gmail.com>
2025-04-23 01:10:27 -0300

    código incrementado

--- calculadora-imc.js
+++ calculadora-imc.js
@@ -1,5 +1,15 @@
-const peso = 76;
-const altura = 1.79;
+const nome ="Rafael"
+const peso = 88.5;
+const altura = 1.83;
 
 const imc = (peso / altura ** 2).toFixed(2);
-console.log(`Seu IMC é ${imc}`);
+
+if (imc <= 18.5) {
+    console.log(`Olá, ${nome}! Você precisa se alimentar mais. Está muito magro.`);
+} else if (imc > 18.5 && imc <= 24.9) {
+    console.log(`Muito bem, ${nome}! Você está com o IMC ideal. Parabéns!!!`);
+} else if (imc > 24.9 && imc < 30) {
+    console.log(`Atenção, ${nome}!!! Você está acima do peso.`);
+} else {
+    console.log(`É, meu fi, tem jeito não. ${nome}, procure uma academia URGENTE!`);
+}
