4918eba5d8d77bb9c309523825608fac6710826d
Bowie <rafael.ps.skt@gmail.com>
2025-04-28 22:55:03 -0300

    Cálculo da soma interna de um hexágono regular

---
+++ somando-angulos-internos.js
@@ -0,0 +1,8 @@
+const ladoPoligno = 6;  // Hexágono
+const somaAnguloInterno = (ladoPoligno - 2) * 180;
+
+console.log(`A soma dos ângulos internos é ${somaAnguloInterno}°`);
+
+const polignoRegular = somaAnguloInterno / ladoPoligno;
+
+console.log(`Cada ângulo interno tem o valor de ${polignoRegular}°`);
