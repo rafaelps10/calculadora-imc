65ce05253992713cded33b195870e40a31e713df
Bowie <rafael.ps.skt@gmail.com>
2025-04-28 23:07:38 -0300

    somando os ângulos internos de um hexágono regular

--- somando-angulos-internos.js
+++ somando-angulos-internos.js
@@ -1,8 +1,8 @@
 const ladoPoligono = 6;  // Hexágono
-const somaAnguloInterno = (ladoPoligno - 2) * 180;
+const somaAnguloInterno = (ladoPoligono - 2) * 180;
 
 console.log(`A soma dos ângulos internos é ${somaAnguloInterno}°`);
 
-const polignoRegular = somaAnguloInterno / ladoPoligno;
+const poligonoRegular = somaAnguloInterno / ladoPoligono;
 
-console.log(`Cada ângulo interno tem o valor de ${polignoRegular}°`);
+console.log(`Cada ângulo interno tem o valor de ${poligonoRegular}°`);
