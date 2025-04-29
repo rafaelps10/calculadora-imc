331bb6a314fa21fe6f9fcfe4ab1c23a3bddda933
Bowie <rafael.ps.skt@gmail.com>
2025-04-28 22:02:51 -0300

    calculando velocidade média em km/h fazendo conversão de grandeza

---
+++ velocidade-media.js
@@ -0,0 +1,6 @@
+const distanciaPercorrida = 1000; // metros
+const tempo = 800; // segundos
+
+const velocidadeMedia = (distanciaPercorrida / tempo) * 3.6; // multiplicando o resultado da divisão por 3,6, o resultado será em metros por segundo
+
+console.log(`${velocidadeMedia} km/h`);
